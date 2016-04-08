a7bc810d9ae083675d0003bd424d3c4933e2c55f
Sefer Kuduzovic <sefer.kuduzovic@gmail.com>
2016-04-08 10:39:54 +0200

    Initial build

---
+++ src/nizmath/Background.java
@@ -0,0 +1,28 @@
+package nizmath;
+
+public class Background {
+	// Metoda za ispisivanje najmanjeg broja u nizu
+	public static void najmanji(int[] niz){
+		
+	}
+	// Metoda za ispisivanje najveceg broja u nizu
+	public static void najveci(int[] niz){
+		
+	}
+	// Metoda za ispisivanje zbira niza
+	public static void zbir(int[] niz){
+		
+	}
+	// Metoda za ispisivanje prosjeka brojeva niza
+	public static void prosjek(int[] niz){
+		
+	}
+	// Metoda za ispisivanje unikatnih brojeva u nizu
+	public static void unikatni(int[] niz){
+		
+	}
+	// Metoda za sortiranje niza od najveæeg ka najmanjeg broja
+	public static void sortiranje(int[] niz){
+		
+	}
+}
