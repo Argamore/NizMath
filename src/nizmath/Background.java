18a601ee3cc005c96b0668070a4bed23b2be1bec
Sefer Kuduzovic <sefer.kuduzovic@gmail.com>
2016-04-08 10:58:23 +0200

    Uradjena metoda za racunanje najmanjeg broja u nizu

--- src/nizmath/Background.java
+++ src/nizmath/Background.java
@@ -3,8 +3,15 @@ package nizmath;
 public class Background {
 	// Metoda za ispisivanje najmanjeg broja u nizu
 	public static void najmanji(int[] niz){
+		int najmanji = niz[0];
+		for (int i = 0; i < niz.length; i++){
+			if (niz[i] < najmanji){
+				najmanji = niz[i];
+			}
+		}
+		System.out.println("Najmanji broj u nizu je: "+najmanji);
+		}
 		
-	}
 	// Metoda za ispisivanje najveceg broja u nizu
 	public static void najveci(int[] niz){
 		
